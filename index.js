42503321737d79288c50e0d3ba46ec7f675c1f75
ncusbisnis-netizen <ncusbisnis@gmail.com>
2026-03-08 22:35:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { default: makeWASocket, useMultiFileAuthState, fetchLatestBaileysVersion } = require("@whiskeysockets/baileys")
+const fs = require("fs")
 
 async function start(){
+
+if(process.env.SESSION && !fs.existsSync("./session/creds.json")){
+const session = JSON.parse(Buffer.from(process.env.SESSION,"base64").toString())
+fs.mkdirSync("./session",{recursive:true})
+fs.writeFileSync("./session/creds.json",JSON.stringify(session,null,2))
+}
 
 const { state, saveCreds } = await useMultiFileAuthState("session")
 const { version } = await fetchLatestBaileysVersion()
@@ -10,18 +17,22 @@ version,
 auth: state
 })
 
-sock.ev.on("connection.update", (update) => {
+sock.ev.on("creds.update", saveCreds)
 
-const { qr } = update
+sock.ev.on("messages.upsert", async ({ messages }) => {
 
-if(qr){
-console.log("SCAN QR INI:")
-console.log(qr)
+const msg = messages[0]
+if(!msg.message) return
+
+const text =
+msg.message.conversation ||
+msg.message.extendedTextMessage?.text
+
+if(text === "ping"){
+await sock.sendMessage(msg.key.remoteJid,{text:"pong 🟢 bot aktif"})
 }
 
 })
-
-sock.ev.on("creds.update", saveCreds)
 
 }
 
